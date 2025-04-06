11d84a579c650e49feeb4a25d197f43e3c52f0a5
Carlo <2023371190@uteq.edu.mx>
2025-04-06 12:58:45 -0600

    fdsf

--- server.js
+++ server.js
@@ -28,7 +28,21 @@ const io = socketIo(server, {
 dotenv.config();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://smartair-api.up.railway.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
+app.options("*", cors()); // habilita preflight para todas las rutas
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+
 
 // Verificar si la carpeta 'uploads' existe; si no, crearla
 const uploadDir = 'uploads/';
